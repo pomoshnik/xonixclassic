30b4b71c7569a824fa6d19ad62690d1ba7e49862
Игорь <igor_sa@list.ru>
2021-01-22 14:03:04 +0300

    1

---
+++ Assets/main.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main : MonoBehaviour
+{
+    public GameObject wall;
+
+    void Start()
+    {
+        PaintStartWall();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void PaintStartWall()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            Instantiate(wall, new Vector3(i, 1, 0), Quaternion.identity);
+            Instantiate(wall, new Vector3(i, 50, 0), Quaternion.identity);
+        }
+
+        for (int j = 1; j <= 50; j++)
+        {
+            Instantiate(wall, new Vector3(1, j, 0), Quaternion.identity);
+            Instantiate(wall, new Vector3(100, j, 0), Quaternion.identity);
+        }
+    }
+}
